bd46136af52717564c0303a5dd078412ea06e307
Dennis <dnpy2009@gmail.com>
2022-11-08 15:15:18 +0800

    update

--- public/room.js
+++ public/room.js
@@ -5,42 +5,21 @@ let counter = document.querySelector("#counter");
 
 roomTemplate.remove();
 
-// async function createRoom() {
-//   const res = await fetch("/room");
-//   const roomArr = await res.json();
-//   console.log("roomArr", roomArr);
-//   for (let room of roomArr) {
-//     console.log("room", room);
-//     let node = roomTemplate.cloneNode(true);
-//     node.querySelector(".room-button").textContent = room.content;
-//     roomContainer.prepend(node);
-//   }
-// }
-
-// createRoom();
-
 function createRoom(input) {
   let roomArr = input;
   roomContainer.textContent = "";
   for (let room of roomArr) {
     let node = roomTemplate.cloneNode(true);
-    node.querySelector(".room-button").textContent = room.content;
+    let str = `room:${room.content}`;
+    node.querySelector(".room-button").innerHTML = str;
     roomContainer.prepend(node);
   }
 }
-
 socket.on("new-room", (value) => {
-  console.log(value);
   createRoom(value);
-  // roomContainer.prepend(node);
 });
 
-socket.on("connect", () => {
-  console.log("connected to socket.io server");
-  callServer();
-});
-
-async function callServer() {
+async function callRoomRouter() {
   let res = await fetch("/room", {
     method: "POST",
     headers: {
@@ -49,3 +28,8 @@ async function callServer() {
     body: JSON.stringify({}),
   });
 }
+
+socket.on("connect", () => {
+  console.log("connected to socket.io server");
+  callRoomRouter();
+});
