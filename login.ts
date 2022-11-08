572dca392f3d0cc45346a557dedaaf7a5c03733b
Hardy <hardy19950616@gmail.com>
2022-11-08 15:35:47 +0800

    login page

--- login.ts
+++ login.ts
@@ -6,12 +6,9 @@ import path from "path";
 const app = express();
 
 app.get("/", (req, res) => {
-  let fullpath = path.resolve("public", "login.html");
-  let halfPath = path.resolve("login.html");
-  console.log({ fullpath, halfPath });
-
   res.sendFile(path.resolve("public", "login.html"));
 });
+
 app.use(express.static("public"));
 const PORT = 8080;
 app.listen(PORT, () => {
