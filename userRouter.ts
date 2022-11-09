3d3cdee6fa3ca2f3814e490e1d7195128a81c1b5
Hardy <hardy19950616@gmail.com>
2022-11-09 09:36:46 +0800

    login register store data

--- userRouter.ts
+++ userRouter.ts
@@ -1,11 +1,29 @@
 import express from "express";
+import { client } from "./database";
 
 //import path from "path";
 //import { Request, Response } from "express";
 
 export const userRouter = express.Router();
 
-userRouter.post("/register", (req, res) => {
+userRouter.post("/login", async (req, res) => {
+  let { username, password } = req.body;
   console.log(req.body);
+  let result = await client.query("select * from users where username=$1", [
+    username,
+  ]);
+  console.log(result);
+  console.log(password);
+  res.end();
+});
+
+userRouter.post("/register", async (req, res) => {
+  let { username, password, email } = req.body;
+
+  await client.query(
+    "INSERT INTO users (username,password,email) values ($1,$2,$3)",
+    [username, password, email]
+  );
+
   res.end();
 });
