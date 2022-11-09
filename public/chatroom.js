1490dbee4e6520dc32e30356c83cb687385e9976
Marcus Kan <marcuskan@Marcusde-MacBook-Pro.local>
2022-11-09 16:09:56 +0800

    chatroom send msg

---
+++ public/chatroom.js
@@ -0,0 +1,79 @@
+const socket = io.connect(); // You can pass in an optional parameter like "http://localhost:8080"
+
+let msg = [
+    {"user_id":1, 
+    "nickname":"Hardy", 
+    "content":"Let's go shopping!",
+    "time":"2:25PM"},
+    {"user_id":2, 
+    "nickname":"Marcus", 
+    "content":"Where shall we go?",
+    "time":"2:28PM"},
+    {"user_id":3, 
+    "nickname":"Dennis", 
+    "content":"MegaBox",
+    "time":"2:32PM"},
+]
+const msgBox = document.querySelector(".message-chat")
+
+let receivedMsg = document.querySelector('#message receiveMsg')
+let sentMsg = document.querySelector('#message my-message')
+
+let user_id = document.querySelector('#user_id')
+
+console.log(user_id);
+
+// send message to the server
+let submitBtn = document.querySelector('.send-message-button')
+let message = document.querySelector('.send-message-text')
+
+let current_user_id;
+
+
+submitBtn.addEventListener('click', () => {
+    console.log(message.value);
+    // fetch('/message', {
+    //     method:'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({message: message.value})
+
+    // })
+
+    socket.emit("user_message", { data: message.value })
+})
+
+socket.on("hello_user", (data) => {
+    // data has the content { msg: "Hello Client" }
+    console.log(data)
+    current_user_id = data.userId;
+})
+
+socket.on("receive_data_from_server", (data) => {
+    // data has the content { msg: "Hello Client" }
+    console.log(data)
+    console.log(data.sendUser);
+    console.log(current_user_id);
+
+
+    msgBox.innerHTML += creatMsgBox(data)
+})
+
+function creatMsgBox(data){
+    return `
+    <div class="message ${ data.sendUser !== current_user_id ? "receiveMsg" : "my-message"}">
+        <div class="message-body">
+            <div class="message-info">
+                <h4> Hardy ${data.sendUser} </h4>
+                <h5> <i class="fa fa-clock-o"></i> 2:25 PM </h5>
+            </div>
+            <hr>
+            <div class="message-text">
+               ${data.receivedData.data}
+            </div>
+        </div>
+        <br>
+    </div>
+    `
+}
--- server.ts
+++ server.ts
@@ -6,6 +6,7 @@ import { topicRouter } from "./topic";
 import { createRoomRouter } from "./room";
 import { userRouter } from "./userRouter";
 import path from "path";
+import expressSession from "express-session";
 
 const app = express();
 const server = new http.Server(app);
@@ -14,9 +15,46 @@ const io = new SocketIO.Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const sessionMiddleware = expressSession({
+  secret: 'Tecky Academy teaches typescript',
+  resave:true,
+  saveUninitialized:true,
+  cookie:{secure:false}
+});
+
+app.use(sessionMiddleware);
+
+io.use( (socket, next) => {
+  let req = socket.request as express.Request
+  let res = req.res as express.Response
+  sessionMiddleware(req, res, next as express.NextFunction)
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("public", "login.html"));
 });
+
+const genId = () => Math.floor( Math.random() * 10000000 )
+io.on('connection', function (socket) {
+
+  const req = socket.request as express.Request;
+  req.session['key'] = genId();
+    
+  (socket.request as any).session.save();
+
+  console.log("Hello a user has enter");
+  socket.join("room-A");
+
+  socket.emit("hello_user" , {data : "hello", userId: req.session['key'] })
+
+  socket.on("user_message" , (data) => {
+    console.log(data, req.session['key'] );
+    io.to("room-A").emit("receive_data_from_server" , { receivedData: data, sendUser: req.session['key']  })
+  })
+  
+});
+
+
 
 app.use(topicRouter);
 app.use(createRoomRouter(io));
@@ -24,6 +62,16 @@ app.use(createRoomRouter(io));
 app.use(userRouter);
 
 app.use(express.static("public"));
+
+app.post('/message',(req,res) => {
+  console.log('creating body', req.body);
+  const { message } = req.body;
+
+  console.log('creating message', message);
+
+
+  res.json({})
+})
 
 app.use((req, res) => {
   res.status(404);
