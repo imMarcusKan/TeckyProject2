1af2e200197a318905ee08f744413caf35d4764e
Marcus Kan <marcuskan@Marcusde-MacBook-Pro.local>
2022-11-17 14:16:29 +0800

    coding modify

--- roomsubmit.ts
+++ roomsubmit.ts
@@ -12,7 +12,7 @@ roomsubmitRouter.post("/demo", async (req, res) => {
     deleteTime = null;
   } else {
     deleteTime = format(
-      add(new Date(), { seconds: addTime }),
+      add(new Date(), { minutes: addTime }),
       "yyyy-MM-dd HH:mm:ss"
     );
   }
