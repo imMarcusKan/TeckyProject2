1bf233015ca2c26ff358321202b80b79a8987699
Hardy <hardy19950616@gmail.com>
2022-11-18 17:18:19 +0800

    finish form and buttom

--- public/edit.js
+++ public/edit.js
@@ -5,10 +5,13 @@ const box = document.querySelector(".box");
 editBtn.addEventListener("click", () => {
   Swal.fire({
     html: `
-        <div>
-        <div class="box">
-        <div class="a">
-        <label class="ChangeProfilePicture">Change Profile Picture</label>
+        <div style="height: 300px;">
+        <div class="select" style="margin-bottom:80px;">
+        <button class="pic" onclick="changeForm('#profile','#editusername','#editpassword')">Change Profile Pic</button>
+        <button class="username"  onclick="changeForm('#editusername','#profile','#editpassword')">Change username</button>
+        <button class="Password" onclick="changeForm('#editpassword','#profile','#editusername')">Change Password</button>
+        </div>
+  
         <form
         id="profile"
         method="post"
@@ -21,15 +24,13 @@ editBtn.addEventListener("click", () => {
         <input type="file" name="image" id="file" />
         <input type="submit" value="submit" id="uploadBtn" />
         </form>
-        <label class="ChangeUsername">Change Username</label>
-        </div>
-        
-        <div class="b">
+
         <form
         id="editusername"
         method="post"
         class="editusername"
         action="/edit-username"
+        style="display:none;"
         >
         <label for="name">New Username:</label>
         <input type="username" id="username" name="username" /><br />
@@ -37,14 +38,14 @@ editBtn.addEventListener("click", () => {
         <button type="submit" id="submit" class="btn">submit</button>
         </div>
         </form>
-        </div>
         
-        <div class="c">
+
         <form
         id="editpassword"
         method="post"
         class="editpassword"
         action="/edit-password"
+        style="display:none;"
         >
         <label for="password">New Password:</label>
         <input type="password" id="password" name="password" /><br />
@@ -54,7 +55,7 @@ editBtn.addEventListener("click", () => {
         <button type="submit" id="submit" class="btn">submit</button>
         </div>
         </form>
-        </div>
+  
         </div>`,
     showCloseButton: true,
     showCancelButton: true,
@@ -86,6 +87,35 @@ editBtn.addEventListener("click", () => {
     }
   });
 });
+
+function changeForm(show, hide1, hide2) {
+  document.querySelector(show).style.display = "unset";
+  document.querySelector(hide1).style.display = "none";
+  document.querySelector(hide2).style.display = "none";
+}
+
+// function changePic() {
+//   const editusername = document.querySelector("#editusername");
+//   const editpassword = document.querySelector("#editpassword");
+//   const pic = document.querySelector("#profile");
+//   editusername.style.display = "none";
+//   editpassword.style.display = "none";
+//   pic.style.display = "unset";
+// }
+
+// function changeUsername() {
+//   const pic = document.querySelector("#profile");
+//   const editpassword = document.querySelector("#editpassword");
+//   pic.style.display = "none";
+//   editpassword.style.display = "none";
+// }
+
+// function changePassword() {
+//   const pic = document.querySelector("#profile");
+//   const editusername = document.querySelector("#editusername");
+//   pic.style.display = "none";
+//   editusername.style.display = "none";
+// }
 
 // const imgDiv = document.querySelector(".profile-pic");
 
